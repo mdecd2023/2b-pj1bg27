52e99c2dbe81776ca974ae05fc35886f2ee53b95
41023245@gm.nfu.edu.tw <41023245@gm.nfu.edu.tw>
2023-03-16 15:15:12 +0800

    編輯

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組員41023248 \n \xa0\xa0\xa0\xa0 41023245 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組員41023248 \n \xa0\xa0\xa0\xa0 41023245 \n 123', 'tags': '', 'url': 'About.html'}]};
